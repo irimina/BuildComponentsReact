fcade97d26a23c7507e75ebe953ed56aa4fb84d3
hungryTechie <iirimina@schools.nyc.gov>
2025-06-29 11:36:43 -0400

    initial commit for Fragmenting a component

---
+++ README.md
@@ -0,0 +1,4 @@
+Title: Wrap multiple elements in a function to diplay on the page
+
+Use fragments <> </> to wrap multiple HTML elements in a function that is part of the component named ListGroup.tsx 
+
---
+++ src/components/ListGroup.tsx
@@ -0,0 +1,19 @@
+function ListGroup(){
+
+    return (
+        <>
+        <h1>List Group Heading</h1>
+            <ul className="list-group">
+                    <li className ="list-group-item">An item</li>
+                    <li className="list-group-item">A second item</li>
+                    <li className="list-group-item">A third item</li>
+                    <li className="list-group-item">A fourth item</li>
+                    <li className="list-group-item">And a fifth one</li>
+            </ul>
+       </>
+    )
+   
+}
+
+export default ListGroup;
+// now we need to add this in our Aoo.tsx
